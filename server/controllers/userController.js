2a7a8b687e13b5976121ffb48b62c1066eebf557
Shivam Vijaywargi <vjshivam5@gmail.com>
2022-11-08 15:07:42 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,10 +2,18 @@ const User = require("../models/user");
 const asyncHandler = require("../utils/asyncHandler");
 const sendToken = require("../utils/sendToken");
 
+// Dummy route
 exports.home = (req, res) => {
   res.send("Hello World");
 };
 
+/*********************************************************
+ * @REGISTER
+ * @route http://localhost:4000/api/v1/register
+ * @method POST
+ * @description User register Controller for signing up the user
+ * @returns User Object
+ *********************************************************/
 exports.registerUser = asyncHandler(async (req, res) => {
   console.log(req.body);
 
@@ -41,6 +49,13 @@ exports.registerUser = asyncHandler(async (req, res) => {
   sendToken(res, user, 201);
 });
 
+/*********************************************************
+ * @LOGIN
+ * @route http://localhost:4000/api/v1/login
+ * @method POST
+ * @description User Login Controller for signing in the user
+ * @returns User Object
+ *********************************************************/
 exports.loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -72,6 +87,14 @@ exports.loginUser = asyncHandler(async (req, res) => {
   sendToken(res, user, 200);
 });
 
+/**********************************************************
+ * @LOGOUT
+ * @route http://localhost:4000/api/v1/logout
+ * @method GET
+ * @description User Logout Controller for logging out the user
+ * @description Removes token from cookies
+ * @returns Success Message "Logged out successfully"
+ **********************************************************/
 exports.logoutUser = asyncHandler(async (req, res) => {
   res.status(200).clearCookie("token").json({
     success: true,
@@ -79,8 +102,22 @@ exports.logoutUser = asyncHandler(async (req, res) => {
   });
 });
 
+/**********************************************************
+ * @USERDASHBOARD
+ * @route http://localhost:4000/api/v1/userDashboard
+ * @method GET
+ * @description check token in cookies, if present then returns user details
+ * @returns Logged In User Details
+ **********************************************************/
 exports.getLoggedInUserDetails = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user);
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User does not exist",
+    });
+  }
 
   res.status(200).json({
     success: true,
